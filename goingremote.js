7bf7d8d222431149813da4292853a329f6677bf2
Dylan Dotolo <dotolo.dylan2@gmail.com>
2016-03-17 21:55:58 -0500

    started http scraper to pull in job data, currently have an error recieving data back

--- goingremote.js
+++ goingremote.js
@@ -1,23 +1,54 @@
+Jobs = new Meteor.Collection('jobs');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    Meteor.call('getJobData', function(error, result) {
+        if (error) {
+            console.log("error", error);
+        }
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+        console.log(result);
+    });
+
+    Template.JobBoard.helpers({
+        // jobs: function() {
+        //     return Jobs.find().fetch();
+        // }
+    });
+
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+      var cheerio = require('cheerio');
+
+      Meteor.methods({
+          getJobData: function() {
+              result = Meteor.http.get("https://www.planetremote.co/");
+              $ = cheerio.load(result.content);
+
+              //html structure to get job data
+              var headers = $('.row > a > .col > h3')
+                .clone() //clone the element
+                .children() //select all the children
+                .remove() //remove all the children
+                .end() //again go back to selected element
+                .text()
+                .split(" / ");
+
+            var data = $('.row > a > .col > h3')
+                .map(function (i) {
+                    var row = {};
+                    $(this).find('a').each(function (i) {
+                        var rowName = headers[i];
+                        row[rowName] = $(this).text();
+                    });
+                    Jobs.insert(row);
+                    return row;
+                }).get();
+
+                return data;
+          },
+      });
   });
 }
