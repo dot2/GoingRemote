e66c17542fbbfbefe8239b344d64afc2a39e1af8
Dylan D <dotolo.dylan2@gmail.com>
2016-03-21 22:21:46 -0500

    Added scraper to pull job details data and redirect back to original source. Added function to push job data into database and check for duplicates

--- goingremote.js
+++ goingremote.js
@@ -1,54 +1,90 @@
 Jobs = new Meteor.Collection('jobs');
 
 if (Meteor.isClient) {
+    //calls the get job function from the server
+    Meteor.call("getJob", function(error, result) {
+    if (error) {
+        console.log("error", error);
+    }
+    console.log(result);
+});
 
-    Meteor.call('getJobData', function(error, result) {
-        if (error) {
-            console.log("error", error);
-        }
-
-        console.log(result);
-    });
+    // Meteor.call("last_action",function(err,result){
+    //     console.log(result); } );
+    // Meteor.call('getJobData', function(error, result) {
+    //     if (error) {
+    //         console.log("error", error);
+    //     }
+    //
+    //     console.log(result);
+    // });
 
     Template.JobBoard.helpers({
         // jobs: function() {
         //     return Jobs.find().fetch();
         // }
+        jobs: ()=> {
+        return Jobs.find({});
+    }
     });
 
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-      var cheerio = require('cheerio');
+        //init cheerio
+         var cheerio = require('cheerio');
 
-      Meteor.methods({
-          getJobData: function() {
-              result = Meteor.http.get("https://www.planetremote.co/");
-              $ = cheerio.load(result.content);
+        Meteor.methods({
+            getJob: function () {
+                result = Meteor.http.get("https://weworkremotely.com/categories/2-programming/jobs#intro");
+                $ = cheerio.load(result.content);
+                var jobResult = [ ];
+                //function to scan and gather data from website
+                var LatestJob = $('ul > li > a').each(function(i, element) {
+                    //Selects each job
+                    var a = $(this);
+                    //gets the job position
+                    var title = a.find('.title').text();
+                    //gets company image
+                    var company = a.find('.company').text();
+                    //get date posted
+                    var date = a.find('.date').text();
+                    //gets the url for the job post
+                    var url = a.attr('href');
+                    //gathers data to push to db
+                    var metadata = {
+                        date: date,
+                        title: title,
+                        company: company,
+                        url: url
+                    };
+                    jobResult.push(metadata);
+                    //insert metadata into mongodb to display to user
+                    // Jobs.insert(metadata);
+                    // return metadata;
+                });
 
-              //html structure to get job data
-              var headers = $('.row > a > .col > h3')
-                .clone() //clone the element
-                .children() //select all the children
-                .remove() //remove all the children
-                .end() //again go back to selected element
-                .text()
-                .split(" / ");
+                for (var i = 0; i < 100; i++) {
+                    var jobPost = jobResult[i];
 
-            var data = $('.row > a > .col > h3')
-                .map(function (i) {
-                    var row = {};
-                    $(this).find('a').each(function (i) {
-                        var rowName = headers[i];
-                        row[rowName] = $(this).text();
-                    });
-                    Jobs.insert(row);
-                    return row;
-                }).get();
+                    var jobAttribues = {
+                            url: jobPost.url,
+                            title: jobPost.title,
+                            company: jobPost.company,
+                            date: jobPost.date
+                    };
+                    jobWithSameLink = Jobs.findOne({url: jobAttribues.url});
 
-                return data;
-          },
-      });
-  });
+                    if (jobAttribues.url && jobWithSameLink) {
+                        throw new Meteor.Error(302,
+                            'This link as already been posted',
+                                jobWithSameLink._id);
+                    }
+                    Jobs.insert(jobAttribues);
+                }
+                return LatestJob;
+            }
+        });
+    });
 }
