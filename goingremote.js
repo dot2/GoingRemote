acdf79691089c0fcdc15ade306ad5e0d04e6b63a
Dylan D <dotolo.dylan2@gmail.com>
2016-03-29 18:14:02 -0500

    working on delete method after 90 days

--- goingremote.js
+++ goingremote.js
@@ -1,11 +1,31 @@
 // Jobs = new Meteor.Collection('jobs');
-
-if (Meteor.isClient) {
-
-
-}
-
-if (Meteor.isServer) {
-    //check to see if post are older than 90 days
-    
-}
+// 
+// if (Meteor.isClient) {
+//     Meteor.call("removeAllJobs");
+//
+// }
+//
+// if (Meteor.isServer) {
+//     //check to see if post are older than 90 days
+//     var clear = function() {
+//         var min = new Date(new Date() - 1);
+//         Jobs.remove({
+//             createdAt: {$lt: min}
+//         });
+//     };
+//     Meteor.startup(function() {
+//         clear();
+//         Meteor.setInterval(clear, 1000000);
+//
+//
+//
+//
+//
+//         // return Meteor.methods({
+//         //     removeAllJobs: function() {
+//         //         return Jobs.remove({createdAt: {$lt: -10}});
+//         //     }
+//         // });
+//     });
+//
+// }
