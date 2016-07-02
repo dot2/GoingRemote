686865c6f638ad1870f16c09ff94458a16c474b9
Dylan D <dotolo.dylan2@gmail.com>
2016-07-02 11:14:36 -0500

    added new job section and started on the join page

--- goingremote.js
+++ goingremote.js
@@ -1,1 +1,5 @@
 
+// UI.registerHelper('shareOnTwitterLink', function() {
+//   return 'https://twitter.com/intent/tweet?url=' + window.location.href + '&text=' +
+//     document.title + "{{homeUrl}}{{url}}";
+// });
